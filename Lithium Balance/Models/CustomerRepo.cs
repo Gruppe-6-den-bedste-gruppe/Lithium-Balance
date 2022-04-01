22cb8ecc1ad203252026ec7982737b83895c1091
lucastran007 <lucastran007@gmail.com>
2022-04-01 13:39:45 +0200

    Add project files.

---
+++ Lithium Balance/Models/CustomerRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lithium_Balance.Views;
+using Lithium_Balance.Models;
+
+namespace Lithium_Balance.Models
+{
+    public class CustomerRepo
+    {
+    }
+}
---
+++ Lithium Balance/Views/NewOrderDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Lithium_Balance.Views;
+
+namespace Lithium_Balance.Views
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class NewOrderDialog : Window
+    {
+        public NewOrderDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
