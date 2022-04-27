71b76d1ad496fe4177d19a93e78d97965ffd9c7f
Ahmad Safwat Shanta <assh38002@edu.ucl.dk>
2022-04-27 12:05:55 +0200

    Oversigt over bestilling i SQL

---
+++ Lithium Balance/Models/DatabaseHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+
+namespace Lithium_Balance.Models
+{
+    public class DatabaseHandler
+    {
+        public List<Order> Orders = new List<Order>();
+        private string connectionString = "Server=10.56.8.36;Database=PEDB06;User Id=PE-06;Password=OPENDB_06";
+
+        public bool IsDbConnected()
+        {
+            string connectionStringLowTimeout = "Server=10.56.8.36;Database=PEDB06;User Id=PE-06;Password=OPENDB_06;Connect Timeout=1";
+            using (SqlConnection con = new SqlConnection(connectionStringLowTimeout))
+            {
+                try
+                {
+                    con.Open();
+                    return true;
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        public List<Order> GetOrderInfo()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT OrderNO, Date, CopmanyName, Receiver, Email, BMSType, SoftwareVersion, LicensDuration FROM Order", connection);
+                using (SqlDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Order order = new Order();
+                        order.OrderNumber = dr["OrderNO"].ToString();
+                        order.OrderDate = DateTime.Parse(dr["Date"].ToString());
+                        order.CompanyName = dr["CopmanyName"].ToString();
+                        order.Receiver = dr["Receiver"].ToString();
+                        order.Email = dr["Email"].ToString();
+                        order.BMSType = dr["BMSType"].ToString();
+                        order.SoftwareVersion = dr["SoftwareVersion"].ToString();
+                        order.LicensDuration = dr["LicensDuration"].ToString();
+
+                        Orders.Add(order);
+
+                    }
+                }
+            }
+            return Orders;
+        }
+        public void SaveOrder(Order order)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Order (OrderNO, Date, CopmanyName, Receiver, Email, BMSType, SoftwareVersion, LicensDuration)" +
+                    "VALUES(@OrderNO, @Date, @CopmanyName, @Receiver, @Email, @BMSType, @SoftwareVersion, @LicensDuration)", connection);
+                command.Parameters.AddWithValue("@OrderNO", order.OrderNumber);
+                command.Parameters.AddWithValue("@Date", order.OrderDate);
+                command.Parameters.AddWithValue("@CopmanyName", order.CompanyName);
+                command.Parameters.AddWithValue("@Recevier", order.Receiver);
+                command.Parameters.AddWithValue("@Email", order.Email);
+                command.Parameters.AddWithValue("@BMSTYpe", order.BMSType);
+                command.Parameters.AddWithValue("@SoftwareVersion", order.SoftwareVersion);
+                command.Parameters.AddWithValue("@LicensDuration", order.LicensDuration);
+
+                connection.Close();
+
+            }
+        }
+    }
+}
---
+++ Lithium Balance/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lithium_Balance.Models
+{
+    public class Order
+    {
+        public string  OrderNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string Receiver { get; set; }
+        public string LicensDuration { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Email { get; set; }
+        public string BMSType { get; set; }
+        public string SoftwareType { get; set; }
+        public string SoftwareVersion { get; set; }
+    }
+}
