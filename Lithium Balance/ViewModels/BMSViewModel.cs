8f130d009f268c52abf29a05c548588d21ba4d2f
Kevin Matin <kema38405@edu.ucl.dk>
2022-05-11 11:01:21 +0200

    BMSViewModel + SoftwareViewModel added.

---
+++ Lithium Balance/ViewModels/BMSViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lithium_Balance.Models;
+using System.Data.SqlClient;
+
+namespace Lithium_Balance.ViewModels
+{
+    internal class BMSViewModel
+    {
+        private readonly string connectionString = "Server=10.56.8.36;Database=PEDB06;User Id=PE-06;Password=OPENDB_06";
+        public BMS CreateBMS(string BMSType, string BMSVersion)
+        {
+            BMS bms = new BMS(BMSType, BMSVersion);
+
+            return bms;
+        }
+
+        public void SaveBMS(BMS bms)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand("INSERT INTO BMS (@BMSType, @BMSVersion)", connection);
+                command.Parameters.AddWithValue("@BMSType", bms.BMSType);
+                command.Parameters.AddWithValue("@BMSVersion", bms.BMSVersion);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+
+    }
+}
