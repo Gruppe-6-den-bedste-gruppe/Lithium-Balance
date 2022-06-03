3f121a4a891eb549265a044e7e237dd6f7bd2743
lucastran007 <lucastran007@gmail.com>
2022-06-03 11:09:56 +0200

    Saving funtion done

--- Lithium Balance/ViewModels/OrderViewModel.cs
+++ Lithium Balance/ViewModels/OrderViewModel.cs
@@ -110,6 +110,42 @@ namespace Lithium_Balance.ViewModels
                 return OrdersList;
             }
         }
+
+        public Order CreateOrder(string OrderNumber, string CompanyName, string Email, string BMSType, string BMSVersion,
+            string SoftwareVersion, string SoftwareType, string LicenseDuration, string Address, string Date)
+        {
+            Order order = new Order(OrderNumber, CompanyName, Email, BMSType, BMSVersion,
+                SoftwareVersion, SoftwareType, LicenseDuration, Address, Date);
+
+            return order;
+        }
+
+        public void SaveOrder(Order order)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand("INSERT INTO orders (orderNumber, licenseDuration, date)" +
+                    " VALUES(@orderNumber, @licenseDuration , @date)" + " Insert into Customer (companyName, email, address)"
+                    + " VALUES(@companyName, @email, @address)" + " insert into bms (bmsType, bmsVersion)" + " VALUES(@bmsType, @bmsVersion)"
+                    + " Insert Into software (softwareType, softwareVersion)" + " VALUES(@softwareType, @softwareVersion)", connection);
+                command.Parameters.AddWithValue("@orderNumber", order.OrderNumber);
+                command.Parameters.AddWithValue("@companyName", order.CompanyName);
+                command.Parameters.AddWithValue("@email", order.Email);
+                command.Parameters.AddWithValue("@bmsType", order.BMSType);
+                command.Parameters.AddWithValue("@bmsVersion", order.BMSVersion);
+                command.Parameters.AddWithValue("@softwareVersion", order.SoftwareVersion);
+                command.Parameters.AddWithValue("@softwareType", order.SoftwareType);
+                command.Parameters.AddWithValue("@licenseDuration", order.LicenseDuration);
+                command.Parameters.AddWithValue("@address", order.Address);
+                command.Parameters.AddWithValue("@date", order.Date);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
     }
         
 
